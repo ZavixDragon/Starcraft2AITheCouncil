fd6909e15ca9190f457f3bf9cae286e0a4bd0e94
noah reinagel <noah.r9@gmail.com>
2018-11-07 18:02:40 -0700

    initial commit

---
+++ Bot/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using SC2APIProtocol;
+
+namespace Bot {
+    internal class Program {
+        // Settings for your bot.
+        private static readonly Bot bot = new RaxBot();
+        private const Race race = Race.Terran;
+
+        // Settings for single player mode.
+//        private static string mapName = "AbyssalReefLE.SC2Map";
+//        private static string mapName = "AbiogenesisLE.SC2Map";
+//        private static string mapName = "FrostLE.SC2Map";
+        private static readonly string mapName = "(2)16-BitLE.SC2Map";
+
+        private static readonly Race opponentRace = Race.Random;
+        private static readonly Difficulty opponentDifficulty = Difficulty.Easy;
+
+        public static GameConnection gc;
+
+        private static void Main(string[] args) {
+            try {
+                gc = new GameConnection();
+                if (args.Length == 0)
+                    gc.RunSinglePlayer(bot, mapName, race, opponentRace, opponentDifficulty).Wait();
+                else
+                    gc.RunLadder(bot, race, args).Wait();
+            }
+            catch (Exception ex) {
+                Logger.Info(ex.ToString());
+            }
+
+            Logger.Info("Terminated.");
+        }
+    }
+}
---
+++ Bot/Wrapper/ProtobufProxy.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.IO;
+using System.Net.WebSockets;
+using System.Threading;
+using System.Threading.Tasks;
+using Google.Protobuf;
+using SC2APIProtocol;
+
+namespace Bot {
+    public class ProtobufProxy {
+        private ClientWebSocket clientSocket;
+        private readonly CancellationToken token = new CancellationTokenSource().Token;
+
+        public async Task Connect(string address, int port) {
+            clientSocket = new ClientWebSocket();
+            // Disable PING control frames (https://tools.ietf.org/html/rfc6455#section-5.5.2).
+            // It seems SC2 built in websocket server does not do PONG but tries to process ping as
+            // request and then sends empty response to client. 
+            clientSocket.Options.KeepAliveInterval = TimeSpan.FromDays(30);
+            var adr = string.Format("ws://{0}:{1}/sc2api", address, port);
+            var uri = new Uri(adr);
+            await clientSocket.ConnectAsync(uri, token);
+
+            await Ping();
+        }
+
+        public async Task Ping() {
+            var request = new Request();
+            request.Ping = new RequestPing();
+            var response = await SendRequest(request);
+        }
+
+        public async Task<Response> SendRequest(Request request) {
+            await WriteMessage(request);
+            return await ReadMessage();
+        }
+
+        public async Task Quit() {
+            var quit = new Request();
+            quit.Quit = new RequestQuit();
+            await WriteMessage(quit);
+        }
+
+        private async Task WriteMessage(Request request) {
+            var sendBuf = new byte[1024 * 1024];
+            var outStream = new CodedOutputStream(sendBuf);
+            request.WriteTo(outStream);
+            await clientSocket.SendAsync(new ArraySegment<byte>(sendBuf, 0, (int) outStream.Position),
+                WebSocketMessageType.Binary, true, token);
+        }
+
+        private async Task<Response> ReadMessage() {
+            var receiveBuf = new byte[1024 * 1024];
+            var finished = false;
+            var curPos = 0;
+            while (!finished) {
+                var left = receiveBuf.Length - curPos;
+                if (left < 0) {
+                    // No space left in the array, enlarge the array by doubling its size.
+                    var temp = new byte[receiveBuf.Length * 2];
+                    Array.Copy(receiveBuf, temp, receiveBuf.Length);
+                    receiveBuf = temp;
+                    left = receiveBuf.Length - curPos;
+                }
+
+                var result = await clientSocket.ReceiveAsync(new ArraySegment<byte>(receiveBuf, curPos, left), token);
+                if (result.MessageType != WebSocketMessageType.Binary)
+                    throw new Exception("Expected Binary message type.");
+
+                curPos += result.Count;
+                finished = result.EndOfMessage;
+            }
+
+            var response = Response.Parser.ParseFrom(new MemoryStream(receiveBuf, 0, curPos));
+
+            return response;
+        }
+    }
+}
